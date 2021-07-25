411753be240b8989c19a8aa5e5c2ae8c6bf03435
NotTastyCupcake <54242753+NotTastyCupcake@users.noreply.github.com>
2021-07-25 17:56:20 +0500

    UploadingPerson

--- WorkerList/View/UploadingPersonsWindow.xaml.cs
+++ WorkerList/View/UploadingPersonsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +25,15 @@ namespace WorkerList.View
         {
             InitializeComponent();
             DataContext = new DataManageVM();
-            UnloadingAddresTextBox.Text = DataManageVM.UnloadingAddres;
+        }
+        public void Browse(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Расширяемый язык разметки(*.xml)| *.xml";
+            if (saveFile.ShowDialog() == true)
+            {
+                saveFile.FileName = UnloadingAddresTextBox.Text;
+            }
         }
     }
 }
--- WorkerList/ViewModel/DataManageVM.cs
+++ WorkerList/ViewModel/DataManageVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -39,7 +40,8 @@ namespace WorkerList.ViewModel
         public static ModelPerson SelectedPerson { get; set; }
 
         //Свойства для выгрузки
-        public static string UnloadingAddres { get; set; } = "person.xml";
+        public static string UnloadingAddres { get; set; } = "d:/persons.xml" ;
+
 
         #endregion
 
